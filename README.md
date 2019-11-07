fb2695263aac6e944f7fe875410b8b3738e797b5
imyeego <imyeego@gmail.com>
2019-11-07 17:17:40 +0800

    update jcenter repos

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 ## promise
+
+---
+
+### import
+- maven
+
+```
+<dependency>
+	<groupId>com.imyeego.promise</groupId>
+	<artifactId>promise-all</artifactId>
+	<version>1.0.0</version>
+	<type>pom</type>
+</dependency>
+```
+- gradle
+
+`implementation 'com.imyeego.promise:promise-all:1.0.0'`
+### usage
+
+```
+Promise.of(()->{
+           Thread.sleep(4000);
+           return "promise";
+        }).then(s -> {
+            // do something
+        }).ui(s -> {
+            // ui option
+        }).make();
+```
--- app/src/main/java/com/liuzhao/promise/MainActivity.java
+++ app/src/main/java/com/liuzhao/promise/MainActivity.java
@@ -3,11 +3,21 @@ package com.liuzhao.promise;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.imyeego.promise.Promise;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Promise.of(()->{
+           Thread.sleep(4000);
+           return "promise";
+        }).then(s -> {
+            // do something
+        }).ui(s -> {
+            // ui option
+        }).make();
     }
 }
