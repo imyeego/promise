1d76ee49c83f8a5a93d569b74175e9135c3a0af3
imyeego <imyeego@gmail.com>
2020-02-14 09:26:29 +0800

    add test code

--- app/src/main/java/com/liuzhao/promise/MainActivity.java
+++ app/src/main/java/com/liuzhao/promise/MainActivity.java
@@ -2,23 +2,29 @@ package com.liuzhao.promise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.imyeego.promise.Fun0;
 import com.imyeego.promise.Promise;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
         Promise.of(()->{
            Thread.sleep(4000);
            return "123";
-        }).map(s -> Integer.parseInt(s)).then(s -> {
-            // do something
+        }).map(Integer::parseInt).then(s -> {
+            Log.e("promise", "" + s);
         }).ui(s -> {
-            // ui option
+            tv.setText(String.valueOf(s + 7));
+            Toast.makeText(MainActivity.this, "" + (s + 7), Toast.LENGTH_SHORT).show();
         }).make();
 
 
